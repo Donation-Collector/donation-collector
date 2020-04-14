cf46551338cca55b6fe8c7e5d7468a5d6d296356
Ruochen Shi <ruochen19@gmail.com>
2020-04-14 12:17:21 -0400

    part of NGO main

--- src/components/NGO-main/NGO-Main-App.js
+++ src/components/NGO-main/NGO-Main-App.js
@@ -1,10 +1,15 @@
 import React, {Component} from 'react';
+import TopBar from "../starter-page/TopBar";
+import AboutUsMain from "../starter-page/About-us-main";
+import NgoMainBody from "./Ngo-main-body";
 
 class NgoMainApp extends Component {
     render() {
         return (
             <div>
-                NGO main here!
+                <TopBar/>
+                <NgoMainBody/>
+                <AboutUsMain/>
             </div>
         );
     }
