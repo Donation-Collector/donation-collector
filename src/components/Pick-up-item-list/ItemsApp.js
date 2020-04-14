f78fd2fc8ab7c721484558ce55ae1394c520a903
muling <mirianva@126.com>
2020-04-14 11:20:41 -0700

    pickup page done

--- src/components/Pick-up-item-list/ItemsApp.js
+++ src/components/Pick-up-item-list/ItemsApp.js
@@ -1,13 +1,89 @@
 import React, {Component} from 'react';
-
+import TopBar from "../starter-page/TopBar";
+import AboutUsMain from "../starter-page/About-us-main";
+import books from "../../assets/images/books.jpg";
+import clothe from "../../assets/images/clothes.jpg";
+import furniture from "../../assets/images/furnitures.jpg";
+import toy from "../../assets/images/Toys.jpg";
+import misce from "../../assets/images/misce.jpg";
 class ItemsApp extends Component {
+    constructor(props) {
+        super();
+
+        this.state = {
+            shopList: [
+                { id: 0, goodName: 'Books', num: 1 , src: books},
+                { id: 1, goodName: 'Clothes',num: 1 , src:clothe},
+                { id: 2, goodName: 'Furnitures',num: 1, src:furniture},
+                { id: 3, goodName: 'Toys',num: 1, src:toy},
+                { id: 4, goodName: 'Others',num: 1, src:misce },
+
+            ]
+        }
+    }
+
+    //修改数据
+    changeNum(index, num) {
+        // console.log(index,num)
+        this.state.shopList[index].num += num;
+
+        this.setState({
+            shopList: this.state.shopList
+        })
+
+    }
+
+    // //删除数据
+    // delect = (index) => {
+    //     //删除数据
+    //     this.state.shopList.splice(index, 1)
+    //
+    //     this.setState({
+    //         shopList: this.state.shopList
+    //     })
+    //
+    // }
+
     render() {
+
         return (
             <div>
-                Donation Item List here!
+                <TopBar/>
+                <h3 className="header">Donation List</h3>
+
+                <div className="DonationListHeader" align="center" >Donation Items</div>
+
+
+                {
+                    this.state.shopList.map((item, key) => {
+                        return (<div className="collection-item" key={item.id}>
+                                <div className="goodname">{item.goodName}</div>
+                                <img src={item.src}  width = "400" height = "300" className="rounded mx-auto d-block" alt="itemImg"/>
+                                <div className="button-position-type">
+                                    <button type="button" className="add-button btn btn-light" onClick={this.changeNum.bind(this, key, -1)} disabled={item.num < 2 ? 'disabled' : ''}>-</button>
+                                    <span className='item-number'>{item.num}</span>
+                                    <button className="subtract btn btn-dark"onClick={this.changeNum.bind(this, key, 1)}>+</button>
+                                </div>
+
+
+
+                            </div>
+                        )
+                    })
+                }
+
+
+                <a href="/n" className="btn btn-secondary starter-ngo-button">Submit Donation</a>
+
+                <AboutUsMain/>
             </div>
-        );
+
+
+        )
     }
 }
 
-export default ItemsApp;
+
+
+export  default  ItemsApp;
+
---
+++ src/components/Pick-up-item-list/shopList.js
@@ -0,0 +1,17 @@
+import books from "../../assets/images/books.jpg";
+import clothe from "../../assets/images/clothes.jpg";
+import furniture from "../../assets/images/furnitures.jpg";
+import toy from "../../assets/images/Toys.jpg";
+import misce from "../../assets/images/misce.jpg";
+
+const iniState =  {
+        shopList:[
+            { id: 0, goodName: 'Books', num: 1 , src: books},
+    { id: 1, goodName: 'Clothes',num: 1 , src:clothe},
+    { id: 2, goodName: 'Furnitures',num: 1, src:furniture},
+    { id: 3, goodName: 'Toys',num: 1, src:toy},
+    { id: 4, goodName: 'Others',num: 1, src:misce }
+
+],
+
+}
