ac463a99065e329c28d95dcdcbd522c8f7fdb204
Ruochen Shi <ruochen19@gmail.com>
2020-05-01 22:28:05 -0400

    home page css change

--- src/components/starter-page/Main-Entry.js
+++ src/components/starter-page/Main-Entry.js
@@ -4,8 +4,11 @@ class MainEntry extends Component {
     render() {
         return (
             <div className="starter-main-body">
-                <a href="/resident" className="btn btn-primary starter-resident-button">Donate Now</a>
-                <a href="/nlogin" className="btn btn-primary starter-ngo-button">NGO Entry</a>
+                <div className="home-button-container">
+                    <a href="/resident" className="btn btn-primary starter-resident-button">Donate Now</a>
+                    <a href="/nlogin" className="btn btn-primary starter-ngo-button">NGO Entry</a>
+                </div>
+
             </div>
         );
     }
